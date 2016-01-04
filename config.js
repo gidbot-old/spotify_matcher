a603e434eac4429dbcb6bdc1c2fd2d809fc9791e
gideon <gideon@blend.la>
2016-01-04 00:06:32 -0500

    changing url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.mongo_url = process.env.COMPOSE_MONGO_URL;
-config.spotify_redirect = 'http://discover-weekly.herokuapp.com/auth/spotify/callback';
+config.spotify_redirect = 'http://discover-weekly.com/auth/spotify/callback';
 
 // config.mongo_url = "mongodb://localhost:27017/spotify";
 // config.spotify_redirect = 'http://localhost:3000/auth/spotify/callback'; 
