80f06450b42df5365e34f76ab91eec4f2f38760e
gideon <gideon@blend.la>
2016-01-06 19:10:58 -0500

    stashing

--- controllers/index.js
+++ controllers/index.js
@@ -261,8 +261,8 @@ router.get('/compare/:facebook_id', function (req, res) {
 							otherDwId: user.dw_spotify_id,
 							currentDisplayName: (user2.display_name)? user2.display_name: user2.spotify_id,
 							otherDisplayName: (user.display_name)? user.display_name: user.spotify_id,
-							currentProfilePic: user2.profile_pic,
-							otherProfilePic: user.profile_pic 
+							currentFacebookId: user2.facebook_id,
+							otherFacebookId: user.facebook_id
 						});
 
 					});
@@ -301,10 +301,24 @@ router.get('/random', function (req, res) {
 		  	var index = Math.floor((Math.random() * (fb_response.data.length-1)));
 		  	res.redirect(302, '/compare/'+ fb_response.data[index].id);
 		  }
-
 		});
 	}
 }); 
+
+router.get('friends', function (req, res) {
+	if (!req.session.facebookId && !req.session.spotifyId) {
+		res.redirect('/login');
+	} else {
+		FB.api('me/friends', 'get', {limit: 200},  function (fb_response) {
+		  if(!fb_response || fb_response.error) {
+		    res.status(500).send('Facebook Error');
+		  } else {
+		  	var index = Math.floor((Math.random() * (fb_response.data.length-1)));
+		  	res.redirect(302, '/compare/'+ fb_response.data[index].id);
+		  }
+		});
+	}
+});
 
 function searchUsers (search, page, callback) { 
 	page = parseInt(page);
@@ -314,7 +328,7 @@ function searchUsers (search, page, callback) {
 	MongoClient.connect(mongoUrl, function (err, db) {
 		db.collection('users').find(
 			{display_name: {$regex:search,  $options: 'i'}},
-			{spotify_id:1, display_name:1, profile_pic: 1},
+			{facebook_id:1, display_name:1},
 			{limit: 10, offset:page}).toArray(function (err, users){
 
 			db.close(); 
