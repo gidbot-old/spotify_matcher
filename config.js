14c0da921dff60b845bf47a462cf2dd91868da59
gideon <gideon@blend.la>
2016-01-07 23:38:44 -0500

    fixing config

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 var config = {};
 
-config.mongo_url = process.env.COMPOSE_MONGO_URL;
-config.spotify_redirect = 'http://discover-weekly.com/auth/spotify/callback';
+// config.mongo_url = process.env.COMPOSE_MONGO_URL;
+// config.spotify_redirect = 'http://discover-weekly.com/auth/spotify/callback';
 
-// config.mongo_url = "mongodb://localhost:27017/spotify";
-config.mongo_url = "mongodb://gidbot:ttfbtb2404@candidate.37.mongolayer.com:11137,candidate.52.mongolayer.com:10829/spotify?replicaSet=set-5689c22023371e1d340008f6";
+config.mongo_url = "mongodb://localhost:27017/spotify";
 config.spotify_redirect = 'http://localhost:3000/auth/spotify/callback'; 
 
 
